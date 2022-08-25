2c609fdc478091f090d85d371aa191c33c63b63f
DuskyElf <91879372+DuskyElf@users.noreply.github.com>
2022-08-25 15:25:54 +0530

    Bug fixed
    Idk why I wrote that silly code the first place

--- tictactoe.py
+++ tictactoe.py
@@ -104,25 +104,28 @@ class Game():
     # Check for the win
     def winCheck(self):
         b = self.board.boardState
-        winner = Board.N
         
         for i in range(3):
             #Horizontal check
-            if b[i][0] == b[i][1] == b[i][2]:
-                winner = b[i][0]
+            if b[i][0] != Board.N:
+                if b[i][0] == b[i][1] and b[i][1] == b[i][2]:
+                    return b[i][0]
             
             #Vertical check
-            if b[0][i] == b[1][i] == b[2][i]:
-                winner = b[0][i]
+            if b[0][i] != Board.N:
+                if b[0][i] == b[1][i] and [1][i] == b[2][i]:
+                    return b[0][i]
         
         # Diagonal checks
-        if b[0][0] == b[1][1] == b[2][2]:
-            winner = b[0][0]
-            
-        if b[2][0] == b[1][1] == b[0][2]:
-            winner = b[2][0]
+        if b[0][0] != Board.N:
+            if b[0][0] == b[1][1] and b[1][1] == b[2][2]:
+                return b[0][0]
         
-        return winner
+        if b[2][0] != Board.N:
+            if b[2][0] == b[1][1] and b[1][1] == b[0][2]:
+                return b[2][0]
+        
+        return Board.N
 
 def main():
     game = Game()
